d69f34a700795e2eac2f86a8f7b4399470e04e23
Captain Bad <cba@freshdrops.io>
2023-03-15 04:35:43 +0000

    Initial theming

--- components/navbar/HamburgerMenu.tsx
+++ components/navbar/HamburgerMenu.tsx
@@ -68,7 +68,7 @@ const HamburgerMenu = () => {
           <Link href="/">
             <Box css={{ width: 34, cursor: 'pointer' }}>
               <Image
-                src="/reservoirLogo.svg"
+                src="/freshdrops-logo-black.svg"
                 width={34}
                 height={39}
                 alt="Reservoir"
